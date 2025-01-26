89c9b1a11b52d6694fe4cb2bf1094ca38d33be78
Ricardo Isaac Hernandez Cruz <lcuperichardhdz@hotmail.com>
2025-01-26 13:53:57 -0600

    Enhance sum_arrays.cpp to allow dynamic array size input and manage memory allocation

--- Tarea1/sum_arrays.cpp
+++ Tarea1/sum_arrays.cpp
@@ -3,7 +3,6 @@
 #include <ctime>
 #include <omp.h>
 
-#define ARRAY_SIZE 1000   // Tamaño de los arreglos
 #define CHUNK 100 // Tamaño del chunk para el bucle for paralelo
 #define SHOW 10   // Número de elementos a mostrar
 
@@ -27,20 +26,38 @@ void fillArray(float *array, int size) {
 }
 
 int main() {
-    std::cout << "Suma de Arreglos en Paralelo\n\n";
+    int array_size;
+    std::cout << "Ingrese el tamaño del array (entre 1000 y 100000): ";
+    std::cin >> array_size;
 
-    float A[ARRAY_SIZE], B[ARRAY_SIZE], C[ARRAY_SIZE];
+    // Limitar el tamaño del array entre 1000 y 100000
+    if (array_size < 1000) {
+        array_size = 1000;
+    } else if (array_size > 100000) {
+        array_size = 100000;
+    }
+
+    std::cout << "Suma de Arreglos en Paralelo\n\n";
+    std::cout << "Tamaño del array: " << array_size << std::endl;
+
+    float *A = new float[array_size];
+    float *B = new float[array_size];
+    float *C = new float[array_size];
     srand(static_cast<unsigned>(time(0))); // Semilla para la generación de números aleatorios
 
     // Llenar los arreglos A y B con valores aleatorios
-    fillArray(A, ARRAY_SIZE);
-    fillArray(B, ARRAY_SIZE);
+    fillArray(A, array_size);
+    fillArray(B, array_size);
+
+    double start_time = omp_get_wtime(); // Tiempo de inicio
 
     // Suma paralela de los arreglos A y B en C
     #pragma omp parallel for shared(A, B, C) schedule(static, CHUNK)
-    for (int index = 0; index < ARRAY_SIZE; index++) {
+    for (int index = 0; index < array_size; index++) {
         C[index] = A[index] + B[index];
     }
+
+    double end_time = omp_get_wtime(); // Tiempo de fin
 
     // Imprimir los primeros elementos de cada arreglo basandose en la constante SHOW
     std::cout << "Primeros " << SHOW << " elementos del array A:" << std::endl;
@@ -54,6 +71,12 @@ int main() {
     std::cout << "Primeros " << SHOW << " elementos del array C (Suma de A y B):" << std::endl;
     printArray(C);
     std::cout << std::endl;
+
+    std::cout << "Tiempo de ejecución: " << (end_time - start_time) << " segundos" << std::endl;
+
+    delete[] A;
+    delete[] B;
+    delete[] C;
 
     return 0;
 }
